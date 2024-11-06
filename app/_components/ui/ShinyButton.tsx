19d46a546e5ec62976f5412a3785942890e81478
Thomas Nguyen <92189845+ThomasN0712@users.noreply.github.com>
2024-11-05 23:57:19 -0800

    Update ShinyButton.tsx

--- app/_components/ui/ShinyButton.tsx
+++ app/_components/ui/ShinyButton.tsx
@@ -10,7 +10,7 @@ const ShinyButton = ({
   iconPosition?: "left" | "right";
 }) => {
   return (
-    <button className="self-start relative z-10 inline-flex gap-2.5 h-12 animate-shimmer items-center justify-center rounded-md border border-dark-700 bg-[linear-gradient(110deg,#1A1A1A,45%,#262626,55%,#1A1A1A)] hover:bg-[linear-gradient(110deg,#262626,45%,#404040,55%,#262626)] bg-[length:200%_100%] px-6 font-medium text-sm sm:text-base text-slate-200 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+    <button className="self-start relative z-10 inline-flex gap-2.5 h-12 animate-shimmer items-center justify-center rounded-md border border-dark-700 bg-[linear-gradient(110deg,#1A1A1A,45%,#262626,55%,#1A1A1A)] hover:bg-[linear-gradient(110deg,#262626,45%,#404040,55%,#262626)] bg-[length:200%_100%] px-6 font-medium text-sm sm:text-base text-slate-200 transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_20px_rgba(66,153,225,0.6)] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
       {iconPosition === "left" && icon}
       {children}
       {iconPosition === "right" && icon}
